62a62e4c96a74b6867976d36b4174300c9988ecf
GREEN <GREEN@G704-13>
2024-08-08 10:56:47 +0900

    Acontroller 리턴구문 수정

--- mysite_17th/src/main/java/com/study/mysite/AController.java
+++ mysite_17th/src/main/java/com/study/mysite/AController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AController {
     @GetMapping("/")
     public String home() {
-        return "Hello, ";
+        return "Devtools과 LIveReload를 사용하면 서버를 껐다 키지 않아도 편리하게 자동으로 서버가 켜지고 브라우저가 refresh 됩니다.";
     }
 }
